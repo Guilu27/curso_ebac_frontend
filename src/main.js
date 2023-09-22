2d80f031f89ca0067f4f412989dcf353f7fa3e0c
Guilherme <guilherme@exemplo.com>
2023-09-22 17:47:44 -0300

    exercicio feito

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicacao(4, 8));
+console.log(saudacao("Gui"));
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+function multiplicacao(a: number, b: number): number {
+  return a * b;
+}
+
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+console.log(multiplicacao(4, 8));
+
+console.log(saudacao("Guilherme"));
